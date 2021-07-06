d3a7850c50e215a0f812e974c47a93478b2a87df
国志 <zgzdream@163.com>
2021-07-06 21:09:16 +0800

    更新github actions

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -56,13 +56,13 @@ module.exports = {
     new OptimizeCSSAssetsPlugin(),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'zhengguozhi.github.io/resume/');
+      // fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'zhengguozhi.github.io/resume/');
 
       // 调用 Chrome 渲染出 PDF 文件
-      const chromePath = findChrome();
-      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${ path.resolve(outputPath, 'resume.pdf') }`,
-        'https://zhengguozhi.github.io/resume/' // 这里注意改成你的在线简历的网站
-      ]);
+      // const chromePath = findChrome();
+      // spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${ path.resolve(outputPath, 'resume.pdf') }`,
+      //   'https://zhengguozhi.github.io/resume/' // 这里注意改成你的在线简历的网站
+      // ]);
     }),
   ]
 };
